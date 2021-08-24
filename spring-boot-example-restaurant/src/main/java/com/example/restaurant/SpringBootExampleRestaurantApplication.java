e441bcd4f2476c477abbc2f79325bfacaf8e1b23
powerhorang2 <tjrdbqls3@naver.com>
2021-08-24 12:26:35 +0900

    네이버 지역검색 API를 활용한 맛집 List 만들기 - naver.yml 추가 했기때문에 그에 따른 설정 수정

--- spring-boot-example-restaurant/src/main/java/com/example/restaurant/SpringBootExampleRestaurantApplication.java
+++ spring-boot-example-restaurant/src/main/java/com/example/restaurant/SpringBootExampleRestaurantApplication.java
@@ -2,12 +2,16 @@ package com.example.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootExampleRestaurantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleRestaurantApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(SpringBootExampleRestaurantApplication.class);
+        springApplicationBuilder.properties("spring.config.location=" + "classpath:/application.yml" + ", classpath:/naver.yml");
+        SpringApplication springApplication = springApplicationBuilder.build();
+        springApplication.run(args);
+    }
 
 }
--- spring-boot-example-restaurant/src/test/java/com/example/restaurant/SpringBootExampleRestaurantApplicationTests.java
+++ spring-boot-example-restaurant/src/test/java/com/example/restaurant/SpringBootExampleRestaurantApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.restaurant;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" + "classpath:/application.yml" + ", classpath:/naver.yml")
 class SpringBootExampleRestaurantApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
