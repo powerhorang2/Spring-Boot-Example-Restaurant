bfb04254c10992b9a3d3776128fb1d02f5abeea2
powerhorang2 <tjrdbqls3@naver.com>
2021-08-24 16:36:27 +0900

    ApiController 에 search 메소드에 @GetMapping 추가 및 add 메소드 추가 , SpringFox Boot Starter Dependence 추가
    search 메소드에 까먹고 GetMapping을 안넣음
    위시리스트 추가를 위한 add method 추가
    api 스펙 확인을 위한 Swagger api 추가

--- spring-boot-example-restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
+++ spring-boot-example-restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
@@ -3,10 +3,10 @@ package com.example.restaurant.controller;
 import com.example.restaurant.wishlist.dto.WishListDto;
 import com.example.restaurant.wishlist.service.WishListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/restaurant")
 @RequiredArgsConstructor
@@ -14,7 +14,15 @@ public class ApiController {
 
     private final WishListService wishListService;
 
+    @GetMapping("/search")
     public WishListDto search(@RequestParam String query) {
         return wishListService.search(query);
+    }
+
+    @PostMapping("")
+    public WishListDto add(@RequestBody WishListDto wishListDto) {
+      log.info("{}", wishListDto);
+
+      return wishListService.add(wishListDto);
     }
 }
