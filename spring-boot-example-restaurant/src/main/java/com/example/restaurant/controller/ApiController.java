60185499e33ea0561bc11d360974eb14c63f22c8
powerhorang2 <tjrdbqls3@naver.com>
2021-08-24 15:41:03 +0900

    검색어를 파라미터로 받는 맛집 검색 Controller 생성

---
+++ spring-boot-example-restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
@@ -0,0 +1,20 @@
+package com.example.restaurant.controller;
+
+import com.example.restaurant.wishlist.dto.WishListDto;
+import com.example.restaurant.wishlist.service.WishListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/restaurant")
+@RequiredArgsConstructor
+public class ApiController {
+
+    private final WishListService wishListService;
+
+    public WishListDto search(@RequestParam String query) {
+        return wishListService.search(query);
+    }
+}
