340ecd04590cef0478d5401138a39c232ae0832f
powerhorang2 <tjrdbqls3@naver.com>
2021-08-24 18:35:49 +0900

    Jap 와 똑같이 Repository의 listAll() -> findAll() 으로 수정 및 방문 체크 로직과 위시리스트 삭제 로직 추가

--- spring-boot-example-restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
+++ spring-boot-example-restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
@@ -24,7 +24,6 @@ public class ApiController {
     @PostMapping("")
     public WishListDto add(@RequestBody WishListDto wishListDto) {
       log.info("{}", wishListDto);
-
       return wishListService.add(wishListDto);
     }
 
@@ -32,4 +31,15 @@ public class ApiController {
     public List<WishListDto> findAll() {
         return wishListService.findAll();
     }
+
+    @DeleteMapping("/{index}")
+    public void delete(@PathVariable int index) {
+        wishListService.delete(index);
+    }
+
+    @PostMapping("/{index}")
+    public void addVisit(@PathVariable int index) {
+        wishListService.addVisit(index);
+    }
+
 }
